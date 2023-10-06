7b81af8b131ac2221b814b0b1a6eccd945284f91
Vitaliy Lyapota <2vitalik@gmail.com>
2023-10-07 01:30:14 +0300

    Minor refactoring changes for easier integration into the project

--- hot_speech/hot_estimator_example.py
+++ hot_speech/hot_estimator_example.py
@@ -7,23 +7,19 @@ import pandas as pd
 from pyquantifier.data import Dataset
 
 
-def main():
-    # ------- Start TODO ---------- #
-    # TODO: load the toxicity scores of a day, as a list
-    cx_list = np.random.rand(10000).tolist()
-    # ------- END TODO ---------- #
-
+def get_hot_values(toxicity_values, platform, metric):
+    """
+    `platform` can be 'reddit' or 'twitter' or 'youtube'
+    `metric` can be 'hate', 'offensive', 'toxic', 'hot'
+    """
     # build a dataset object from the list
-    num_items = len(cx_list)
+    num_items = len(toxicity_values)
     df = pd.DataFrame.from_dict({'uid': list(range(num_items)), 'pos': cx_list, 'neg': 1-np.array(cx_list)})
     dataset = Dataset(df=df, labels=['pos', 'neg'])
 
     # load the calibration curve of the hot speech dataset
     cached_dists_filepath = 'hot_speech/hot_cached_dists.pkl'
     cached_dists = pickle.load(open(cached_dists_filepath, 'rb'))
-
-    platform = 'reddit'  # platform can be 'reddit' or 'twitter' or 'youtube'
-    metric = 'hot'  # metric can be 'hate', 'offensive', 'toxic', 'hot'
 
     calibration_curve = cached_dists[f'{platform}_{metric}_calibration_curve']
     ex_prevalence_est = dataset.extrinsic_estimate(calibration_curve=calibration_curve)
@@ -33,5 +29,9 @@ def main():
     # in_prevalence_est = dataset.intrinsic_estimate(class_conditional_densities=class_conditional_densities)
     # print(f'intrinsic estimate: {in_prevalence_est:.4f} on the a simulated data')
 
+    return ex_prevalence_est
+
+
 if __name__ == '__main__':
-    main()
+    cx_list = np.random.rand(10000).tolist()
+    print(get_hot_values(cx_list, 'reddit', 'hot'))
