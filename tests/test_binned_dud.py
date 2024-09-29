cc0498d247a326fa99d906344a24f785ed5d4f41
swu2 <swu2@iu.edu>
2024-09-29 17:12:15 -0400

    update test cases with unittest

--- tests/test_binned_dud.py
+++ tests/test_binned_dud.py
@@ -1,15 +1,27 @@
+import unittest
 from pyquantifier.distributions import BinnedDUD
 
-
-# generate a unit test for the BinnedDUD class
-def test_binned_dud():
-    data = ['apple', 'apple', 'banana', 'pear', 'pear', 'pear'] * 100
-    dud = BinnedDUD(data)
-    dud.plot(ci=True)
-    print(dud.sample(3))
-    print(dud.get_density('apple'))
-    print(dud.get_density('banana'))
-    print(dud.get_density('pear'))
+test_case_list = [
+    (['apple', 'apple', 'banana', 'pear', 'pear', 'pear'] * 100,
+     {'apple': 0.3333, 'banana': 0.1667, 'pear': 0.5})
+    ]
 
 
-test_binned_dud()
+class TestBinnedDudDensity(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_binned_dud_density(self):
+        for case_num, test_case in enumerate(test_case_list):
+            data, expected_result = test_case
+            dud = BinnedDUD(data)
+            with self.subTest(msg=f'Checking case number {case_num+1}'):
+                for label, density in expected_result.items():
+                    self.assertAlmostEqual(dud.get_density(label), 
+                                           density, 
+                                           places=3, 
+                                           msg=f'Test failed: density of label "{label}" does not match expected result')
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_multinomial_dud.py
+++ tests/test_multinomial_dud.py
@@ -1,16 +1,27 @@
+import unittest
 from pyquantifier.distributions import MultinomialDUD
 
-
-# generate a unit test for the MultinomialDUD class
-def test_multinomial_dud():
-    labels = ['apple', 'banana', 'pear']
-    probs = [0.7, 0.1, 0.2]
-    dud = MultinomialDUD(labels, probs)
-    dud.plot()
-    print(dud.generate_data(10))
-    print(dud.get_density('apple'))
-    print(dud.get_density('banana'))
-    print(dud.get_density('pear'))
+test_case_list = [
+    (['apple', 'banana', 'pear'],
+     [0.7, 0.1, 0.2],
+     {'apple': 0.7, 'banana': 0.1, 'pear': 0.2})
+    ]
 
 
-test_multinomial_dud()
+class TestMultinomialDUDDensity(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_multinomial_dud_density(self):
+        for case_num, test_case in enumerate(test_case_list):
+            labels, probs, expected_result = test_case
+            dud = MultinomialDUD(labels, probs)
+            with self.subTest(msg=f'Checking case number {case_num+1}'):
+                for label, density in expected_result.items():
+                    self.assertEqual(dud.get_density(label), 
+                                     density, 
+                                     msg=f'Test failed: density of label "{label}" does not match expected result')
+
+
+if __name__ == "__main__":
+    unittest.main()
